78172a922d7b452bba43af4b1833c9d12b9a9c7e
myfreeer <myfreeer@users.noreply.github.com>
2017-12-30 16:12:11 +0800

    libressl: switch to release
    builds much faster than git master

--- packages/libressl.cmake
+++ packages/libressl.cmake
@@ -1,8 +1,7 @@
 ExternalProject_Add(libressl
-    GIT_REPOSITORY https://github.com/libressl-portable/portable.git
-    GIT_SHALLOW 1
-    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/libressl-*.patch
-    UPDATE_COMMAND ""
+    URL "http://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.6.4.tar.gz"
+    URL_HASH SHA256=638a20c2f9e99ee283a841cd787ab4d846d1880e180c4e96904fc327d419d11f
+    PATCH_COMMAND ${EXEC} sed -i '/SUBDIRS = crypto ssl tls include apps tests man/ c\\SUBDIRS = crypto ssl tls include\\' Makefile.am Makefile.in
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
@@ -12,6 +11,4 @@ ExternalProject_Add(libressl
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-force_rebuild_git(libressl)
-autogen(libressl)
 extra_step(libressl)
