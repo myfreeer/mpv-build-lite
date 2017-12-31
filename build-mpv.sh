41147dbe19c37193b2ac8615fee62d32dad8c04e
myfreeer <myfreeer@users.noreply.github.com>
2017-12-31 10:01:13 +0800

    appveyor: workaround missing build cache

--- build-mpv.sh
+++ build-mpv.sh
@@ -10,6 +10,15 @@ cd ..
 
 mkdir -p ./build64
 cd ./build64
+echo 'int main(){return 0;}' > gcctest.c
+install/bin/x86_64-w64-mingw32-gcc.exe gcctest.c || {
+pacman -S p7zip --noconfirm --needed
+wget -nv -Otoolchain.7z https://ci.appveyor.com/api/projects/myfreeer/mpv-build-lite/artifacts/toolchain.7z?branch=toolchain
+mkdir -p install
+cd install
+7z x -y ../toolchain.7z
+cd ..
+}
 cmake -DTARGET_ARCH=x86_64-w64-mingw32 -G Ninja ..
 ninja mpv
 cd ..
