4b1eaa2fca7a8b9d173d6aeca131a9023be61ed2
myfreeer <myfreeer@users.noreply.github.com>
2018-02-14 19:57:09 +0800

    packages: replace sourceforge urls
    Unavailable to wget.

--- packages/libmodplug.cmake
+++ packages/libmodplug.cmake
@@ -1,6 +1,7 @@
 ExternalProject_Add(libmodplug
-    URL "http://download.sourceforge.net/modplug-xmms/libmodplug-0.8.9.0.tar.gz"
-    URL_HASH SHA256=457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de
+    GIT_REPOSITORY https://github.com/Konstanty/libmodplug.git
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
@@ -10,5 +11,6 @@ ExternalProject_Add(libmodplug
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
+force_rebuild_git(libmodplug)
 extra_step(libmodplug)
 autoreconf(libmodplug)
--- packages/zlib.cmake
+++ packages/zlib.cmake
@@ -1,5 +1,5 @@
 ExternalProject_Add(zlib
-    URL "http://download.sourceforge.net/libpng/zlib-1.2.11.tar.gz"
+    URL "https://zlib.net/zlib-1.2.11.tar.gz"
     URL_HASH SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
     PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/zlib-1-win32-static.patch
     CONFIGURE_COMMAND ${EXEC} CHOST=${TARGET_ARCH} <SOURCE_DIR>/configure
