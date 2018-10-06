4245eeb354e3d9801728e012da40a2e13c5778f2
shinchiro <shinchiro@users.noreply.github.com>
2018-10-06 17:26:43 +0800

    libressl: switch to cmake & ninja build

--- packages/libressl.cmake
+++ packages/libressl.cmake
@@ -1,13 +1,15 @@
 ExternalProject_Add(libressl
-    URL "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.7.4.tar.gz"
-    URL_HASH SHA256=1e3a9fada06c1c060011470ad0ff960de28f9a0515277d7336f7e09362517da6
-    PATCH_COMMAND ${EXEC} sed -i '/SUBDIRS = crypto ssl tls include apps tests man/ c\\SUBDIRS = crypto ssl tls include\\' Makefile.am Makefile.in
-    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
-        --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
+    URL "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.8.1.tar.gz"
+    URL_HASH SHA256=334bf7050f1db4087feebb30571ec13d9fa975bf05d6003ce3ab6d7d2452cf42
+    CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
+        -G Ninja
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+        -DLIBRESSL_APPS=OFF
+        -DLIBRESSL_TESTS=OFF
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
