rorgoroth/mingw-cmake-env

ffmpeg: whitespaces nits

packages/ffmpeg.cmake
@@ -28,50 +28,50 @@ ExternalProject_Add(ffmpeg
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
-    --cross-prefix=${TARGET_ARCH}-
-    --prefix=${MINGW_INSTALL_PREFIX}
-    --arch=${TARGET_CPU}
-    --target-os=mingw32
-    --target-exec=wine
-    --pkg-config-flags=--static
-    --enable-cross-compile
-    --enable-runtime-cpudetect
-    --enable-gpl
-    --enable-version3
-    --enable-nonfree
-    --enable-avresample
-    --enable-postproc
-    --enable-avisynth
-    --enable-gmp
-    --enable-libass
-    --enable-libbluray
-    --enable-libfreetype
-    --enable-libfribidi
-    --enable-libgme
-    --enable-libmodplug
-    --enable-libmp3lame
-    --enable-libopus
-    --enable-libsoxr
-    --enable-libspeex
-    --enable-libvorbis
-    --enable-libvpx
-    --enable-libx264
-    --enable-libaom
-    --enable-libdav1d
-    --enable-libxvid
-    --enable-libzimg
-    --enable-libtls
-    --enable-libxml2
-    --enable-libmysofa
-    --enable-cuda
-    --enable-cuvid
-    --enable-nvdec
-    --enable-nvenc
-    --enable-libmfx
-    --enable-amf
-    --disable-decoder=libaom_av1
-    --disable-w32threads
-    "--extra-cflags=-DMODPLUG_STATIC"
+        --cross-prefix=${TARGET_ARCH}-
+        --prefix=${MINGW_INSTALL_PREFIX}
+        --arch=${TARGET_CPU}
+        --target-os=mingw32
+        --target-exec=wine
+        --pkg-config-flags=--static
+        --enable-cross-compile
+        --enable-runtime-cpudetect
+        --enable-gpl
+        --enable-version3
+        --enable-nonfree
+        --enable-avresample
+        --enable-postproc
+        --enable-avisynth
+        --enable-gmp
+        --enable-libass
+        --enable-libbluray
+        --enable-libfreetype
+        --enable-libfribidi
+        --enable-libgme
+        --enable-libmodplug
+        --enable-libmp3lame
+        --enable-libopus
+        --enable-libsoxr
+        --enable-libspeex
+        --enable-libvorbis
+        --enable-libvpx
+        --enable-libx264
+        --enable-libaom
+        --enable-libdav1d
+        --enable-libxvid
+        --enable-libzimg
+        --enable-libtls
+        --enable-libxml2
+        --enable-libmysofa
+        --enable-cuda
+        --enable-cuvid
+        --enable-nvdec
+        --enable-nvenc
+        --enable-libmfx
+        --enable-amf
+        --disable-decoder=libaom_av1
+        --disable-w32threads
+        "--extra-cflags=-DMODPLUG_STATIC"
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1

myfreeer/mpv-build-lite

packages/ffmpeg.cmake
@@ -19,52 +19,51 @@ ExternalProject_Add(ffmpeg
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
-    --cross-prefix=${TARGET_ARCH}-
-    --prefix=${MINGW_INSTALL_PREFIX}
-    --arch=${TARGET_CPU}
-    --target-os=mingw32
-    --target-exec=wine
-    --pkg-config-flags=--static
-    --enable-cross-compile
-    --enable-runtime-cpudetect
-    --enable-gpl
-    --enable-version3
-    --enable-nonfree
-    --enable-avresample
-    --enable-postproc
-    --enable-avisynth
-    --enable-gmp
-    --enable-libass
-    --enable-libbluray
-    --enable-libfreetype
-    --enable-libfribidi
-    --enable-libgme
-    --enable-libmodplug
-    --enable-libmp3lame
-    --enable-libopus
-    --enable-libsoxr
-    --enable-libspeex
-    --enable-libvorbis
-    --enable-libx264
-#    --enable-libaom
-#    --disable-encoder=libaom_av1
-    --enable-libxvid
-    --enable-libzimg
-    --enable-schannel
-    --enable-libxml2
-    --enable-cuda
-    --enable-cuvid
-    --enable-nvdec
-    --enable-nvenc
-    --enable-libmysofa
-    --enable-amf
-    --enable-libmfx
-    --disable-w32threads
-    --disable-debug
-    --disable-doc
-    --disable-programs
-    "--extra-libs='-lsecurity -lschannel'" # ffmpeg’s build system is retarded
-    "--extra-cflags=-DMODPLUG_STATIC"
+        --cross-prefix=${TARGET_ARCH}-
+        --prefix=${MINGW_INSTALL_PREFIX}
+        --arch=${TARGET_CPU}
+        --target-os=mingw32
+        --target-exec=wine
+        --pkg-config-flags=--static
+        --enable-cross-compile
+        --enable-runtime-cpudetect
+        --enable-gpl
+        --enable-version3
+        --enable-nonfree
+        --enable-avresample
+        --enable-postproc
+        --enable-avisynth
+        --enable-gmp
+        --enable-libass
+        --enable-libbluray
+        --enable-libfreetype
+        --enable-libfribidi
+        --enable-libgme
+        --enable-libmodplug
+        --enable-libmp3lame
+        --enable-libopus
+        --enable-libsoxr
+        --enable-libspeex
+        --enable-libvorbis
+        --enable-libx264
+#       --enable-libaom
+        --enable-libxvid
+        --enable-libzimg
+        --enable-schannel
+        --enable-libxml2
+        --enable-cuda
+        --enable-cuvid
+        --enable-nvdec
+        --enable-nvenc
+        --enable-libmysofa
+        --enable-amf
+        --enable-libmfx
+        --disable-w32threads
+        --disable-debug
+        --disable-doc
+        --disable-programs
+        "--extra-libs='-lsecurity -lschannel'" # ffmpeg’s build system is retarded
+        "--extra-cflags=-DMODPLUG_STATIC"
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1