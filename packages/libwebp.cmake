0ed29fc86e4498aa565d1ea0bfd935508633fe18
myfreeer <myfreeer@users.noreply.github.com>
2019-10-02 14:15:11 +0800

    libwebp: fix

--- packages/libwebp.cmake
+++ packages/libwebp.cmake
@@ -2,12 +2,12 @@ ExternalProject_Add(libwebp
     GIT_REPOSITORY https://chromium.googlesource.com/webm/libwebp.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND  <SOURCE_DIR>/configure
-        --host=
-        --prefix=
+    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
+        --host=${TARGET_ARCH}
+        --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
-    BUILD_COMMAND 
-    INSTALL_COMMAND  install
+    BUILD_COMMAND ${MAKE}
+    INSTALL_COMMAND ${MAKE} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
