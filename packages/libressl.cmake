bb8b1e71e7a3faf43a29c89a5efe24d890f33f5c
shinchiro <shinchiro@users.noreply.github.com>
2020-06-14 12:31:45 +0800

    libressl: move to tarball release
    the git version produced error with twitter's video, while tarball doesn't. Weird
    Fix #79

--- packages/libressl.cmake
+++ packages/libressl.cmake
@@ -1,6 +1,7 @@
 ExternalProject_Add(libressl
-    URL "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.8.1.tar.gz"
-    URL_HASH SHA256=334bf7050f1db4087feebb30571ec13d9fa975bf05d6003ce3ab6d7d2452cf42
+    URL https://cloudflare.cdn.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.2.0.tar.gz
+    URL_HASH SHA256=47bd2eb4b4503e47c02efa7e67d2fcd95c7eac6bc9d06b343a1b4705793ed1d5
+    PATCH_COMMAND patch -p1 -i ${CMAKE_CURRENT_SOURCE_DIR}/libressl-0001-remove-postfix-in-libs-name.patch
     CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -G Ninja
         -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
@@ -8,9 +9,11 @@ ExternalProject_Add(libressl
         -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
         -DLIBRESSL_APPS=OFF
         -DLIBRESSL_TESTS=OFF
+        -DBUILD_SHARED_LIBS=OFF
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
+autoreconf(libressl)
 extra_step(libressl)
