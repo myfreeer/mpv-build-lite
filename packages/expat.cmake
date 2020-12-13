918ace5d3001197eb1e0b51aa8cc1b651c99dfcb
myfreeer <myfreeer@users.noreply.github.com>
2020-12-13 19:15:47 +0800

    expat: update to 2.2.10 (#100)

--- packages/expat.cmake
+++ packages/expat.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(expat
-    URL "https://github.com/libexpat/libexpat/releases/download/R_2_2_9/expat-2.2.9.tar.xz"
-    URL_HASH SHA256=1ea6965b15c2106b6bbe883397271c80dfa0331cdf821b2c319591b55eadc0a4
+    URL "https://download.sourceforge.net/expat/expat-2.2.10.tar.bz2"
+    URL_HASH SHA256=b2c160f1b60e92da69de8e12333096aeb0c3bf692d41c60794de278af72135a5
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
